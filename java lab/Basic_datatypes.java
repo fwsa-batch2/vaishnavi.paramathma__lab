0e98c9552891ad09027c601040276747535dba9b
vaishnavi-paramathma <93571103+vaishnavi-paramathma@users.noreply.github.com>
2022-04-05 16:00:08 +0530

    Add files via upload

---
+++ java lab/Basic_datatypes.java
@@ -0,0 +1,30 @@
+public class Basic_datatypes{
+    public static void main(String[] args){
+        boolean bool = true;
+        byte number1 = -128;
+        byte number2 = 127;
+        short number3 = -32768;
+        short number4 = 32765;
+        int number5 = - 2147483648;
+        int number6 = 2147483647;
+        long number7 = 9223372036854775807L;
+        long number8 = -9223372036854775808L;
+        float number9 = 111.4f;
+        double number10 = 65.6;
+        char alphabet = 'a';
+
+        System.out.println(bool);
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+        System.out.println(number7);
+        System.out.println(number8);
+        System.out.println(number9);
+        System.out.println(number10);
+        System.out.println(alphabet);
+
+    }
+}
