2d7e3c6a329fcf4872c25b89eee347ef671597d5
vaishnavi-2003 <vaishnavi22082003@gmail.com>
2022-05-05 19:52:26 +0530

    added a file

---
+++ java lab/FehrenheitToCelcius.java
@@ -0,0 +1,8 @@
+public class FehrenheitToCelcius{
+    public static void main(String[] args) {
+        float fahenheit, celcius;
+        fahenheit=65;
+        celcius=((fahenheit-32)*5)/9;
+        System.out.println("the temperature in celcius is"+celcius);
+    }
+}
